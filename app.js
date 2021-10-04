3aad615ebc7e187bcb2b8e30371bc79bf39a280b
nathalycossio <deboracossio@gmail.com>
2021-10-03 23:41:44 -0300

    adding ccss and images

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 const express = require('express');
+const { resolve } = require('path');
 const app = express();
 const path = require('path');
+
+
+const publicPath = path.resolve(__dirname, './public');
+app.use(express.static(publicPath));
+
 
 app.listen(3000, () => {
     console.log('servidor corriendo');
 });
 
+app.get('/', function(req,res){
+    res.sendFile(path.resolve(__dirname, './views/home.html'));  
+});
+
 /*app.get('/', function(req,res){
-    res.sendFile(path.join(__dirname , '/views/home.html'));  
+    res.send('Hola');   
 });*/
 
 app.get('/404', function(req,res){
     res.send('Error página no encontrada');   
 });
 
-app.get('/', function(req,res){
-    res.send('Hola');   
-});
